404c79ec83ff4b582ac35f5c1ef2382281c47360
Tomoki Oke(LITALICO) <tomoki.oke@litalico.co.jp>
2020-06-07 03:50:35 +0900

    :green_apple:prefecture.presence

--- app/app/models/postcode_datum.rb
+++ app/app/models/postcode_datum.rb
@@ -2,4 +2,5 @@ class PostcodeDatum < ApplicationRecord
     include CsvParsable
 
     validates :postcode, presence: true, format: { with: /\A\d{7}\z/ }
+    validates :prefecture_name, presence: true
 end
